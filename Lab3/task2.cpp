c4a9b3256c50869f40fbd4a6deb32b1b9c3e52ac
Alex <alex.tereshkov01@mail.ru>
2020-11-09 00:43:20 +0300

    Update task2.cpp

--- Lab3/task2.cpp
+++ Lab3/task2.cpp
@@ -1,11 +1,46 @@
 #include <iostream>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <string>
+#include <vector>
 using namespace std;
 
-int main (int argc, char** argv)
+void recursive_fork(int count)
 {
+    if( count <= 0) return;
+    pid_t pid;
+    vector<int> pids;
+    for(int i = 0; i < count; i++)
+    {
+        pid = fork();
+        if (pid == -1)
+        {
+            perror("Fork Error\n");
+            exit(1);
+        }
+        else if(pid == 0)
+        {
+            sleep(1);
+            recursive_fork(count - 1);
+            sleep(10 + count);
+            return;
+        }
+        else 
+        {
+            pids.push_back(pid);
+        }
+    }
+    cout << getpid() << ": ";
+    for (size_t i = 0; i < count; i++)
+    {
+        cout << pids[i] << " ";
+    }
+    cout << endl;
+}
+
+int main (int argc, char** argv)
+{   
     if(argc == 1)
     {
         perror("No input\n");
@@ -16,27 +51,14 @@ int main (int argc, char** argv)
         perror("Wrong input\n");
         exit(-2);
     }
-    pid_t pid;
-    int count = stoi(argv[1]);
-    for(int j = 0; j < 5; j++)
+
+    pid_t startpid = getpid();
+    recursive_fork(5);
+    if (getpid() == startpid) 
     {
-        for(int i = j; i < 5; i++)
-        {
-            pid = fork();
-            if (pid == -1)
-            {
-                perror("Fork Error\n");
-                exit(1);
-            }
-            else if(pid == 0)
-            {
-                cout << "Child  PID: " << getpid() << "    " << i << endl;
-                return 0;
-            }
-            else 
-            {
-                cout << "Parent PID: " << getpid() << "    " << i << endl;
-            }
-        }
+        sleep(6);
+        string command = "pstree -p > ";
+        command+= argv[1];
+        system(command.c_str());
     }
 }
