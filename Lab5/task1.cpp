8ee5ab536027e89ae46fcac37e3d0b6077f150eb
Alex <alex.tereshkov01@mail.ru>
2021-01-04 22:19:38 +0300

    Update task1.cpp

--- Lab5/task1.cpp
+++ Lab5/task1.cpp
@@ -10,22 +10,20 @@
 
 using namespace std;
 
-double f(double x)
+long double f(long double x)
 {
-    return 4/(1 + x*x);
+    return 4.0/(1.0 + x*x);
 }
 
-double integral_calc(double lb, double rb, int quantity, double func(double))
+long double integral_calc(long double lb, long double rb, long double quantity, long double func(long double))
 {
-    double sum = 0;
-    double step = (rb - lb) / (double) quantity;
-    double i = lb + step / 2; 
+    long double sum = 0.0;
+    long double step = (rb - lb) / quantity;
 
-    for(int i = 0; i < quantity; i++)
+    for(long double i = 0.0; i < quantity; i++)
     {
         sum += func(lb + i * step);
     }
-
     return sum / quantity;
 }
 
@@ -37,27 +35,26 @@ int main (int argc, char **argv)
         exit(1);
     }
 
-    int     fifo[2];
+    int fifo[2];
     if(pipe(fifo) == -1)
     {
         perror("Pipe error");
         exit(2);
     }
 
-    pid_t   pid;
-    string  arg1 = argv[1];
-    string  arg2 = argv[2];
-    string  tmp;
-    char    buf[BUFSIZ];
-    int     quantity_of_processes = atoi(arg1.c_str());
-    int     intervals_per_process = atoi(arg2.c_str()) / quantity_of_processes;
-    int     precission = arg2.length();
-    double  lb = 0;
-    double  rb = 1;
-    double  step = (rb - lb) / (double)quantity_of_processes;
-    double  sum = 0;
+    pid_t           pid;
+    string          arg1 = argv[1];
+    string          arg2 = argv[2];
+    long double     tmp;
+    long double     quantity_of_processes = atoi(arg1.c_str());
+    long double     intervals_per_process = atoi(arg2.c_str()) / quantity_of_processes;
+    long double     precission = arg2.length();
+    long double     lb = 0;
+    long double     rb = 1;
+    long double     step = (rb - lb) / quantity_of_processes;
+    long double     sum = 0;
 
-    for(int i = 0; i < quantity_of_processes; i++)
+    for(long double i = 0; i < quantity_of_processes; i++)
     {
         pid = fork();
         if(pid < 0)
@@ -67,20 +64,20 @@ int main (int argc, char **argv)
         }
         else if(!pid)
         {
-            tmp = to_string(integral_calc((double)i * step, (double)(i + 1) * step - 1 / pow(10, precission), intervals_per_process, f));
+            tmp = integral_calc(i * step, (i + 1) * step - 1 / pow(10, precission), intervals_per_process, f);
             close(fifo[0]);
-            write(fifo[1], tmp.c_str(), BUFSIZ);
+            write(fifo[1], &tmp, sizeof(long double));
             close(fifo[1]);
             exit(1);
         }
         else
         {
-            waitpid(pid, 0, 0);
-            read(fifo[0], buf, BUFSIZ);
-            sum += atof(buf);
+            read(fifo[0], &tmp, sizeof(long double));
+            sum += tmp;
         }
     }
-    sum /= (double) quantity_of_processes;
+    sum /=  quantity_of_processes;
+    sleep(1);
     cout << "Calculated: " << setprecision(precission) << sum << endl;
     cout << "Known:      " << setprecision(precission) << M_PI << endl;
 }
